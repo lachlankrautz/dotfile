3815874b61c4984e8acbdcdbb00faf1e50f002b8
Lachlan Krautz <lachlan.krautz@gmail.com>
2017-06-12 10:40:06 +1000

    spaces

--- install.sh
+++ install.sh
@@ -5,68 +5,68 @@ install_dotfile() {
 
     local WINDOWS=0
     if [[ "${OS}" =~ .*indows.* ]]; then
-	SUDO_CMD=""
-	WINDOWS=1
+    SUDO_CMD=""
+    WINDOWS=1
     else
-	if [ "${EUID}" -eq 0 ]; then
+    if [ "${EUID}" -eq 0 ]; then
             echo "Do not install as root; user home dir is needed"
             return 1
-	fi
+    fi
     fi
 
     if [ ! -d /opt ]; then
-	echo "Missing install dir \"/opt\""
-	return 1
+    echo "Missing install dir \"/opt\""
+    return 1
     fi
 
     if [ -d /opt/dotfile ]; then
-	echo "Uninstalling old version"
-	${SUDO_CMD} rm -rf /opt/dotfile
-	if [ ! "${?}" -eq 0 ]; then
-	    echo "Unable to remove old version"
-	    return 1
-	fi
-	echo
+    echo "Uninstalling old version"
+    ${SUDO_CMD} rm -rf /opt/dotfile
+    if [ ! "${?}" -eq 0 ]; then
+        echo "Unable to remove old version"
+        return 1
+    fi
+    echo
     fi
 
     if [ ! -d /tmp ]; then
-	echo "Missing temp dir"
-	return 1
+    echo "Missing temp dir"
+    return 1
     fi
 
     if [ -d /tmp/dotfile ]; then
-	rm -rf /tmp/dotfile
-	if [ ! "${?}" -eq 0 ]; then
-	    echo "Unable to clear temp"
-	    return 1
-	fi
+    rm -rf /tmp/dotfile
+    if [ ! "${?}" -eq 0 ]; then
+        echo "Unable to clear temp"
+        return 1
+    fi
     fi
 
     echo "Downloading"
     git clone --depth=1 --branch=master git@github.com:lachlankrautz/dotfile /tmp/dotfile
     if [ ! "${?}" -eq 0 ]; then
-	echo "Unable to clone project"
-	return 1
+    echo "Unable to clone project"
+    return 1
     fi
     echo
 
     rm -rf /tmp/dotfile/.git /tmp/dotfile/.gitignore
     if [ ! "${?}" -eq 0 ]; then
-	echo "Unable to clean up git files"
-	return 1
+    echo "Unable to clean up git files"
+    return 1
     fi
 
     chmod 755 /tmp/dotfile/bin/dotfile
     if [ ! "${?}" -eq 0 ]; then
-	echo "Unable to set executable permissions"
-	return 1
+    echo "Unable to set executable permissions"
+    return 1
     fi
 
     echo "Moving to /opt/dotfile"
     ${SUDO_CMD} mv /tmp/dotfile /opt
     if [ ! "${?}" -eq 0 ]; then
-	echo "Unable to move project to /opt"
-	return 1
+    echo "Unable to move project to /opt"
+    return 1
     fi
     echo
 
@@ -74,40 +74,40 @@ install_dotfile() {
     local NEED_LINK=0
     local LINK=$(readlink /usr/bin/dotfile)
     if [ ! -z "${LINK}" ] && [ ! "${LINK}" = "/opt/dotfile/bin/dotfile" ]; then
-	NEED_LINK=1
-	echo "Existing link points elsewhere, removing"
-	${SUDO_CMD} rm /usr/bin/dotfile
-	if [ ! "${?}" -eq 0 ]; then
-	    echo "Unable to remove bad link"
-	    return 1
-	fi
+    NEED_LINK=1
+    echo "Existing link points elsewhere, removing"
+    ${SUDO_CMD} rm /usr/bin/dotfile
+    if [ ! "${?}" -eq 0 ]; then
+        echo "Unable to remove bad link"
+        return 1
+    fi
     fi
     if [ ! -f /usr/bin/dotfile ]; then
-	NEED_LINK=1
+    NEED_LINK=1
     fi
     if [ "${NEED_LINK}" -eq 1 ]; then
 
-	echo "Creating system link"
-	if [ "${WINDOWS}" -eq 1 ]; then
-	    local WIN_SRC="$(cygpath -w "/opt/dotfile/bin/dotfile")"
-	    local WIN_DEST="$(cygpath -w "/usr/bin/dotfile")"
-	    local CMD_C="mklink ${WIN_DEST} ${WIN_SRC}"
-	    cmd /C "\"${CMD_C}\"" > /dev/null 2>&1
-	else
-	    ${SUDO_CMD} ln -s /opt/dotfile/bin/dotfile /usr/bin/dotfile
-	fi
+    echo "Creating system link"
+    if [ "${WINDOWS}" -eq 1 ]; then
+        local WIN_SRC="$(cygpath -w "/opt/dotfile/bin/dotfile")"
+        local WIN_DEST="$(cygpath -w "/usr/bin/dotfile")"
+        local CMD_C="mklink ${WIN_DEST} ${WIN_SRC}"
+        cmd /C "\"${CMD_C}\"" > /dev/null 2>&1
+    else
+        ${SUDO_CMD} ln -s /opt/dotfile/bin/dotfile /usr/bin/dotfile
+    fi
 
-	if [ ! "${?}" -eq 0 ]; then
-	    echo "Unable to create system link"
-	    return 1
-	fi
+    if [ ! "${?}" -eq 0 ]; then
+        echo "Unable to create system link"
+        return 1
+    fi
     fi
 
     if [ -L /usr/bin/dotfile ] && [ "$(readlink /usr/bin/dotfile)" = "/opt/dotfile/bin/dotfile" ]; then
-	echo "Link confirmed"
+    echo "Link confirmed"
     else
-	echo "Unable to install link"
-	return 1
+    echo "Unable to install link"
+    return 1
     fi
     echo
 

d63604e0a3fb036914eb7681c6a1d162177f7009
Lachlan Krautz <lachlan.krautz@gmail.com>
2018-12-28 02:31:07 +1000

    stub export

--- src/command/export.sh
+++ src/command/export.sh
@@ -1,6 +1,40 @@
 #!/usr/bin/env bash
 
-command_export() {
-    echo "exporting ..."
+title_export() {
+    doc_title << 'EOF'
+EXPORT
+
+EOF
     return 0
+}
+
+command_export() {
+    local PATTERN="${1##*/}"
+    local SEARCH_DIR_PART="${1/${PATTERN}/}"
+    local SEARCH_DIR
+    SEARCH_DIR="$(abspath "${HOME}/$(relpath "${SEARCH_DIR_PART}" "${HOME}")")"
+
+    title_export
+
+    if [ -z "${PATTERN}" ]; then
+        echo "Missing file pattern"
+        echo
+        return 1
+    fi
+
+    cdd "${HOME}"
+    export_dotfiles_pattern "${SEARCH_DIR}" "${PATTERN}" "${GROUP}"
+    return "${?}"
+    echo "exporting ..."
+
+    echo
+    return 0
+}
+
+export_dotfiles_pattern() {
+    local SEARCH_DIR="${1}"
+    local PATTERN="${2}"
+    local GROUP="${3}"
+
+    info "Export ${term_fg_blue}${SEARCH_DIR}/${PATTERN}${term_reset} into ${term_fg_blue}${DOTFILES_DIR}/${GROUP}${term_reset}"
 }
